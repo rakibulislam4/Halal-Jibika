d9cc9e6cb7fb640817b7f323c4ca866282b0aea7
Rakibul Islam <wowboyrabbi@gmail.com>
2023-12-31 19:41:38 +0600

    home page fully done

--- src/Components/LatestJobs/LatestJobs.jsx
+++ src/Components/LatestJobs/LatestJobs.jsx
@@ -1,9 +1,81 @@
-import './LatestJobs.css'
-export default function LatestJobs()
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from "react";
+import "./LatestJobs.css";
+import { useNavigate } from "react-router-dom";
+// Creating an array to store internship objects
+const internships = [
+  {
+    id: 1,
+    title: 'Google Internship Program',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png',
+    companyName: 'Google',
+    position: 'Intern Developer',
+    description: 'This is an internship opportunity from Google'
+  },
+  {
+    id: 2,
+    title: 'Microsoft Internship Program',
+    logo: 'https://download.logo.wine/logo/Microsoft/Microsoft-Logo.wine.png',
+    companyName: 'Microsoft',
+    position: 'Software Engineering Intern',
+    description: 'This is an internship opportunity from Microsoft'
+  },
+  {
+    id: 4,
+    title: 'Apple Internship Program',
+    logo: 'https://www.svgrepo.com/show/69341/apple-logo.svg',
+    companyName: 'Apple',
+    position: 'Software Engineering Internship',
+    description: 'This is an internship opportunity from Apple'
+  },
+  {
+    id: 5,
+    title: 'Facebook Internship Program',
+    logo: 'https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/facebook-round-color-icon.png',
+    companyName: 'Facebook',
+    position: 'Software Developer Intern',
+    description: 'This is an internship opportunity from Facebook'
+  },
+  {
+    id: 6,
+    title: 'NASA Internship Program',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1224px-NASA_logo.svg.png',
+    companyName: 'NASA',
+    position: 'Space Science Intern',
+    description: 'This is an internship opportunity from NASA'
+  }
+];
+export default function LatestJobs() {
 
 
-{
+
+const navigate = useNavigate()
   return (
-    <div>LatestJobs</div>
-  )
+    <div>
+      <div className="job-list">
+        {internships &&
+          internships.map((data) => (
+            <div className="single-job">
+              <img
+                src={data.logo}
+                alt=""
+              />
+              <div className="job-details">
+                <h3>{data.title}</h3>
+                <strong>{data.companyName}</strong>
+                <p>{data.position}</p>
+                <button  onClick={() => navigate("/jobs")}>
+                  Apply Now
+                </button>
+              </div>
+              <div className="salary">
+                <p>
+                  $2500 / <span>Month</span>
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
--- src/Routes/SignUp/SignUp.jsx
+++ src/Routes/SignUp/SignUp.jsx
@@ -1,7 +1,66 @@
+import { FaGithub } from 'react-icons/fa'
 import './SignUp.css'
+import { FcGoogle } from 'react-icons/fc'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>SignUp</div>
+    <>
+      <div className="sign-in-main">
+        <div className="sign-in">
+          <div className="sign-in-container">
+            <h1>Sign Up</h1>
+            <p style={{ marginBottom: "1rem" }}>
+             Get access to all the features of Halal Jibika
+            </p>
+            <div className="sign-in-form">
+              <form onSubmit={handleSubmit}>
+                <div className="form">
+                <input placeholder="Full Name"
+                name='name' type="text" />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <button
+                    className="sign-in-btn"
+                    type="submit"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </form>
+
+              <p style={{ marginTop: "0.5rem" }}>
+                Already have an account? <Link to="/login">Sign In </Link>
+                instead or go to <Link
+                 to="/">home</Link>
+              </p>
+            </div>
+            <p style={{ marginTop: "0.5rem" }}>Sign-in or Sign-Up with</p>
+            <div className="sign-in-icons">
+              <button className="google">
+                <FcGoogle />
+              </button>
+              <button
+          
+                className="github"
+              >
+                <FaGithub />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
