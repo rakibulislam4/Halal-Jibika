d1f27d585ddc27c66b870f05edc39e8ff35ed19e
Rakibul Islam <wowboyrabbi@gmail.com>
2024-02-02 15:05:10 +0600

    deleted unnecery local storege bug fixed for home screen not supported in all devices

--- src/customHooks/useLocalItems.js
+++
@@ -1,9 +0,0 @@
-
-export const useLocalItems = () => {
-        const data = localStorage.getItem('userData');
-        if(data){
-            const parsedData = JSON.parse(data);
-            const { userData, user } = parsedData;
-            return { userData, user };
-        }
-}
